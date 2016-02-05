e3b5e9d8d4c6fffd76420bd227f946e08c05995d
Andres Vera <raul.andres.vp@gmail.com>
2016-02-05 11:59:24 -0500

    js input validation and tmdb curl call

--- search.js
+++ search.js
@@ -1,18 +1,26 @@
-function search(str) {
-  var xhttp;
-  if (str.length == 0) { 
-    document.getElementById("output").innerHTML = "";
+function search() {
+  var xhttp = new XMLHttpRequest();
+  var input = document.getElementById("input").value;
+  // console.log("input: ",input);
+  if (!validateSearchInput(input)) { 
     return;
   }
-  // console.log("str: ",str);
-  xhttp=new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       renderOutput(xhttp);
     }
   };
-  xhttp.open("GET", "http://localhost/~andresvera/Alert-Logic/search.php?q="+str, true);
+  xhttp.open("GET", "http://localhost/~andresvera/Alert-Logic/search.php?input="+input, true);
   xhttp.send();
+}
+
+function validateSearchInput(str) {
+  var pattern = /^([a-zA-Z]+ [a-zA-Z]+)/;
+  if (!pattern.test(str)) { 
+    alert("Please enter the first and last name of an actor.");
+    return false;
+  }
+  return true;
 }
 
 function renderOutput(xhttp) {
--- search.php
+++ search.php
@@ -1,29 +1,36 @@
 <?php
-$a[] = "Anna";
-$a[] = "Amanda";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Vicky";
+  $url_search = "https://api.themoviedb.org/3/search/person?&query=".urlencode($_REQUEST["input"]);
+  $object_search = new tmdb_api_call($url_search);
+  $arr_movies_search = json_decode($object_search->get_movies(),true);
+  echo var_dump($arr_movies_search);
 
-$q = $_REQUEST["q"];
+  /**
+   *  This class consumes the TMDb API services depending on the URL 
+   *  passed on as reference to the class constructor in order to  
+   *  get necessary info about movies.
+   */
+  class tmdb_api_call {
+    protected $url;
+    const api_key = "da978a15e0922d1624da0150e1a1fe19";
 
-$hint = "";
-
-if ($q !== "") {
-    $q = strtolower($q);
-    $len=strlen($q);
-    foreach($a as $name) {
-        if (stristr($q, substr($name, 0, $len))) {
-            if ($hint === "") {
-                $hint = $name;
-            } else {
-                $hint .= ", $name";
-            }
-        }
+    /**
+     *  Class constructor
+     */
+    public function __construct($url) {
+        $this->url = $url;
     }
-}
 
-echo $hint === "" ? "no suggestion" : $hint;
+    /**
+     *  This method makes GET requests with curl to the TMDb API.
+     */
+    public function get_movies() {
+      $request =  $this->url.'&api_key='.self::api_key; 
+      $ch = curl_init($request);
+      curl_setopt($ch, CURLOPT_HEADER, false); 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      $curl_response = curl_exec($ch); 
+      curl_close($ch); 
+      return $curl_response;
+    }
+  }
 ?>
